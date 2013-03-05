8b9b61be6e43b1b2dfc286d23ce4b8f56fe5b939
Thomas Weber <webedhmb@mailbox.tu-berlin.de>
2013-03-05 02:26:07 +0100

    little reflection demo extension

--- BattlestationAntaris/Battlestation Antaris/Battlestation Antaris/Tools/ReflectionDemo.cs
+++ BattlestationAntaris/Battlestation Antaris/Battlestation Antaris/Tools/ReflectionDemo.cs
@@ -6,6 +6,14 @@ namespace Battlestation_Antaris.Tools
     public class ReflectionDemo
     {
 
+        public abstract class ReflectiveCall<T, V>
+        {
+            public T obj;
+            public String methodName;
+            public Object[] parameters;
+            public abstract String resultFormat(V obj);
+        }
+
         public static void PrintAttribute(Object obj, String attributeName)
         {
             FieldInfo info = obj.GetType().GetField(attributeName);
@@ -16,6 +24,13 @@ namespace Battlestation_Antaris.Tools
         {
             MethodInfo info = obj.GetType().GetMethod(methodName);
             return info.Invoke(obj, parameters);
+        }
+
+
+        public static String CallMethod<T, V>(ReflectiveCall<T, V> refCall)
+        {
+            V result = (V)CallMethod(refCall.obj, refCall.methodName, refCall.parameters);
+            return refCall.resultFormat(result);
         }
 
     }
