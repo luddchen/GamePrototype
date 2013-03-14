283824c465ea6e2dfacbfe7a5b8ee0ebd721b6bb
Ludde <ludwig.ringler@mailbox.tu-berlin.de>
2013-03-14 10:05:11 +0100

    AI XML reader/writer cleanup

--- BattlestationAntaris/Battlestation Antaris/Battlestation Antaris/Control/AI/AI_XML.cs
+++ BattlestationAntaris/Battlestation Antaris/Battlestation Antaris/Control/AI/AI_XML.cs
@@ -16,62 +16,46 @@ namespace Battlestation_Antaris.Control.AI
             XmlTextWriter writer = new XmlTextWriter(fileName, null);
 
             writer.WriteStartDocument();
-            //writer.WriteComment("AI Container");
             writer.Formatting = Formatting.Indented;
             writer.WriteStartElement("AI");
 
-            writer.WriteStartElement("Items");
-            foreach (AI_Item item in aiContainer.aiItems)
-            {
-                writer.WriteStartElement("Item");
+                writer.WriteStartElement("Items");
+                foreach (AI_Item item in aiContainer.aiItems)
+                {
+                    writer.WriteStartElement("Item");
+                        writer.WriteAttributeString("Type", item.GetType().ToString());
 
-                    writer.WriteStartElement("Type");
-                        writer.WriteString(item.GetType().ToString());
-                    writer.WriteEndElement();
-
-                    writer.WriteStartElement("SubType");
-                        writer.WriteString(item.subType.ToString());
-                    writer.WriteEndElement();
-
-                    writer.WriteStartElement("Position");
-                        writer.WriteStartElement("X");
-                            writer.WriteString(item.abstractPosition.X.ToString());
+                        writer.WriteStartElement("SubType");
+                            writer.WriteString(item.subType.ToString());
                         writer.WriteEndElement();
-                        writer.WriteStartElement("Y");
-                            writer.WriteString(item.abstractPosition.Y.ToString());
-                        writer.WriteEndElement();
-                    writer.WriteEndElement();
 
+                        writer.WriteStartElement("Position");
+                            writer.WriteAttributeString("x", item.abstractPosition.X.ToString());
+                            writer.WriteAttributeString("y", item.abstractPosition.Y.ToString());
+                        writer.WriteEndElement();
+
+                    writer.WriteEndElement();
+                }
                 writer.WriteEndElement();
-            }
-            writer.WriteEndElement();
 
-            writer.WriteStartElement("Connections");
-            foreach (AI_Connection connection in aiContainer.aiConnections)
-            {
-                writer.WriteStartElement("Connection");
+                writer.WriteStartElement("Connections");
+                foreach (AI_Connection connection in aiContainer.aiConnections)
+                {
+                    writer.WriteStartElement("Connection");
 
-                    writer.WriteStartElement("Source");
-                        writer.WriteStartElement("ItemIndex");
-                            writer.WriteString(aiContainer.aiItems.IndexOf(connection.getSource().item).ToString());
+                        writer.WriteStartElement("Source");
+                            writer.WriteAttributeString("Item", aiContainer.aiItems.IndexOf(connection.getSource().item).ToString());
+                            writer.WriteAttributeString("Port", connection.getSource().item.outputs.IndexOf(connection.getSource()).ToString());
                         writer.WriteEndElement();
-                        writer.WriteStartElement("PortIndex");
-                            writer.WriteString(connection.getSource().item.outputs.IndexOf(connection.getSource()).ToString());
+
+                        writer.WriteStartElement("Target");
+                            writer.WriteAttributeString("Item", aiContainer.aiItems.IndexOf(connection.getTarget().item).ToString());
+                            writer.WriteAttributeString("Port", connection.getTarget().item.inputs.IndexOf(connection.getTarget()).ToString());
                         writer.WriteEndElement();
+
                     writer.WriteEndElement();
-
-                    writer.WriteStartElement("Target");
-                        writer.WriteStartElement("ItemIndex");
-                            writer.WriteString(aiContainer.aiItems.IndexOf(connection.getTarget().item).ToString());
-                        writer.WriteEndElement();
-                        writer.WriteStartElement("PortIndex");
-                            writer.WriteString(connection.getTarget().item.inputs.IndexOf(connection.getTarget()).ToString());
-                        writer.WriteEndElement();
-                    writer.WriteEndElement();
-
+                }
                 writer.WriteEndElement();
-            }
-            writer.WriteEndElement();
 
             writer.WriteEndElement();
             writer.WriteEndDocument();
@@ -87,76 +71,59 @@ namespace Battlestation_Antaris.Control.AI
 
             while (reader.Read())
             {
-                switch (reader.NodeType)
+                if (reader.NodeType == XmlNodeType.Element)
                 {
-                    case XmlNodeType.Element :
-                        if (reader.Name == "Item")
-                        {
-                            AI_Item item = null;
+                    if (reader.Name == "Item")
+                    {
+                        AI_Item item = null;
 
-                            reader.Read();// Type Node
-                            reader.Read();
-                            Type type = Type.GetType(reader.ReadString());
-                            Object[] parameters = new Object[3];
-                            parameters[0] = new Vector2( 0.5f, 0.5f);
-                            parameters[1] = HUDType.RELATIV;
-                            parameters[2] = game;
-                            item = (AI_Item)Activator.CreateInstance(type, parameters);
+                        Type type = Type.GetType(reader.GetAttribute(0));
+                        Object[] parameters = new Object[3];
+                        parameters[0] = new Vector2( 0.5f, 0.5f);
+                        parameters[1] = HUDType.RELATIV;
+                        parameters[2] = game;
+                        item = (AI_Item)Activator.CreateInstance(type, parameters);
 
-                            reader.Read();// SubType Node
-                            reader.Read();
-                            Type subType = item.subType.GetType();
-                            item.subType = Enum.Parse(subType, reader.ReadString());
-                            item.subTypeString.String = item.subType.ToString().Replace('_',' ');
+                        ContinueToNode(reader, "SubType");
+                        Type subType = item.subType.GetType();
+                        item.subType = Enum.Parse(subType, reader.ReadString());
+                        item.subTypeString.String = item.subType.ToString().Replace('_',' ');
 
-                            reader.Read();// Position Node
-                            reader.Read();
-                            reader.Read();// X Node
-                            reader.Read();
-                            item.abstractPosition.X = float.Parse(reader.ReadString());
-                            reader.Read();// Y Node
-                            reader.Read();
-                            item.abstractPosition.Y = float.Parse(reader.ReadString());
+                        ContinueToNode(reader, "Position");
+                        item.abstractPosition.X = float.Parse(reader.GetAttribute(0));
+                        item.abstractPosition.Y = float.Parse(reader.GetAttribute(1));
 
-                            aiContainer.Add(item);
-                        }
-                        if (reader.Name == "Connection")
-                        {
-                            AI_Connection connection = new AI_Connection(game);
+                        aiContainer.Add(item);
 
-                            reader.Read();// Source Node
-                            reader.Read();
-                            reader.Read();// Item Index Node
-                            reader.Read();
-                            int sourceItemIndex = int.Parse(reader.ReadString());
-                            reader.Read();// Port Index Node
-                            reader.Read();
-                            int sourcePortIndex = int.Parse(reader.ReadString());
+                    }
 
-                            reader.Read();
-                            reader.Read();
-                            reader.Read();
-                            reader.Read();// Target Node
-                            reader.Read();
-                            reader.Read();// Item Index Node
-                            reader.Read();
-                            int targetItemIndex = int.Parse(reader.ReadString()); 
-                            reader.Read();// Port Index Node
-                            reader.Read();
-                            int targetPortIndex = int.Parse(reader.ReadString());
+                    if (reader.Name == "Connection")
+                    {
+                        AI_Connection connection = new AI_Connection(game);
 
-                            connection.setSource(aiContainer.aiItems[sourceItemIndex].outputs[sourcePortIndex]);
-                            connection.setTarget(aiContainer.aiItems[targetItemIndex].inputs[targetPortIndex]);
+                        ContinueToNode(reader, "Source");
+                        int sourceItemIndex = int.Parse(reader.GetAttribute(0));
+                        int sourcePortIndex = int.Parse(reader.GetAttribute(1));
 
-                            aiContainer.aiConnections.Add(connection);
-                        }
+                        ContinueToNode(reader, "Target");
+                        int targetItemIndex = int.Parse(reader.GetAttribute(0));
+                        int targetPortIndex = int.Parse(reader.GetAttribute(1));
 
-                        break;
+                        connection.setSource(aiContainer.aiItems[sourceItemIndex].outputs[sourcePortIndex]);
+                        connection.setTarget(aiContainer.aiItems[targetItemIndex].inputs[targetPortIndex]);
+
+                        aiContainer.aiConnections.Add(connection);
+                    }
                 }
             }
 
-            Console.WriteLine();
             reader.Close();
+        }
+
+
+        private static void ContinueToNode(XmlTextReader reader, String nodeName)
+        {
+            while (reader.Read() && reader.Name != nodeName) { }
         }
 
 
